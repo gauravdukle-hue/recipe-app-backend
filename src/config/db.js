a15cad63348554cdcc5b97d298b1a651b959a0e8
gauravdukle-hue <Gaurav.dukle@gmail.com>
2026-08-30 10:25:14 -0500

    Use DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,11 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const db = new Pool({
-  user: process.env.DB_USER || 'admin',
-  password: process.env.DB_PASSWORD || 'password',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'recipe_app',
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // needed for Railway
+  }
 });
 
-db.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
-
-export default db;
+export default pool;
